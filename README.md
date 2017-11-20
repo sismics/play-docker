93bddc0d79e90cf2ff15fee34b70cdbcf9093ec4
Jean-Marc Tremeaux <jm.tremeaux@sismics.com>
2017-11-20 08:50:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# play-docker plugin
+
+This plugin adds [Docker](https://www.docker.com/) support to Play! Framework 1 applications.
+
+# How to use
+
+####  Add the dependency to your `dependencies.yml` file
+
+```
+require:
+    - docker -> docker 0.1.0
+
+repositories:
+    - sismics:
+        type:       http
+        artifact:   "http://release.sismics.com/repo/play/[module]-[revision].zip"
+        contains:
+            - docker -> *
+
+```
+####  Run the `play deps` command
+
+####  Add the configuration to `application.conf`
+
+```
+docker.remote_host=yourdockerhost.com
+docker.remote_port=2376
+docker.cert_dir=/your/cert/dir
+```
+
+####  Basic authentication
+
+Alternately, you can authenticate to the Docker Engine using basic auth:
+
+```
+docker.authorization=base64_encoded
+```
+
+####  Registry authentication
+
+If you need to authenticate to the Docker Registry, add the following configuration parameters: 
+
+```
+docker.registry.username=username
+docker.registry.password=password
+docker.registry.address=registry_address
+```
+
+# License
+
+This software is released under the terms of the Apache License, Version 2.0. See `LICENSE` for more
+information or see <https://opensource.org/licenses/Apache-2.0>.
---
+++ app/helpers/docker/DockerUtil.java
@@ -0,0 +1,81 @@
+package helpers.docker;
+
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerCertificates;
+import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.auth.FixedRegistryAuthSupplier;
+import com.spotify.docker.client.messages.RegistryAuth;
+import com.spotify.docker.client.messages.RegistryConfigs;
+import play.Play;
+
+import java.net.URI;
+import java.nio.file.Paths;
+
+/**
+ * Docker utilities.
+ *
+ * @author jtremeaux
+ */
+public class DockerUtil {
+
+    /**
+     * Create a new Docker client from Play! parameters.
+     *
+     * @return New Docker client
+     */
+    public static DockerClient createDockerClient() {
+        try {
+            DefaultDockerClient.Builder builder = DefaultDockerClient.builder();
+            if (getDockerCertDir() != null) {
+                builder
+                        .uri(URI.create("https://" + getDockerRemoteHost() + ":" + getDockerRemotePort("2376")))
+                        .dockerCertificates(DockerCertificates.builder()
+                                .dockerCertPath(Paths.get(getDockerCertDir()))
+                                .build().orNull());
+            } else {
+                builder.uri(URI.create("http://" + getDockerRemoteHost() + ":" + getDockerRemotePort("2400")));
+                if (getDockerAuthorization() != null) {
+                    builder.header("Authorization", "Basic " + getDockerAuthorization());
+                }
+            }
+            if (getRegistryAddress() != null) {
+                builder.registryAuthSupplier(new FixedRegistryAuthSupplier(RegistryAuth.builder()
+                        .username(getRegistryUserName())
+                        .password(getRegistryPassword())
+                        .serverAddress(getRegistryAddress())
+                        .build(), RegistryConfigs.empty()));
+            }
+            return builder.build();
+        } catch (Exception e) {
+            throw new RuntimeException("Error building Docker Client", e);
+        }
+    }
+
+    private static String getDockerCertDir() {
+        return Play.configuration.getProperty("docker.cert_dir");
+    }
+
+    private static String getDockerRemoteHost() {
+        return Play.configuration.getProperty("docker.remote_host");
+    }
+
+    private static String getDockerRemotePort(String defaultPort) {
+        return Play.configuration.getProperty("docker.remote_port", defaultPort);
+    }
+
+    private static String getDockerAuthorization() {
+        return Play.configuration.getProperty("docker.authorization");
+    }
+
+    private static String getRegistryUserName() {
+        return Play.configuration.getProperty("docker.registry.username");
+    }
+
+    private static String getRegistryPassword() {
+        return Play.configuration.getProperty("docker.registry.password");
+    }
+
+    private static String getRegistryAddress() {
+        return Play.configuration.getProperty("docker.registry.address");
+    }
+}
